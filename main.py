f0d3bbd6508d7dc554222db272e91c9ac67ea8bb
Jagan Prem <notjagan@gmail.com>
2017-01-18 12:47:51 -0500

    Add code that should have been pushed yesterday

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def get_team_history(team_key):
     # TODO
     pass
 
+events = int(input("Number of events to stalk: "))
+
+if events < 1:
+    exit(0)
+
 event_id = input("Event ID #1 (including year): ") + "/"
 event_info = loads(requests.get(base_url + "events/" + event_id + "teams", headers=headers).text)
 teams = []
@@ -19,9 +24,14 @@ keys = {}
 for team_info in event_info:
     key = team["key"]
     keys.add(key)
+    teams.append(get_team_history(key))
 
-event_id = input("Event ID #1 (including year): ")
-event_info = loads(requests.get(base_url + event_id + "/teams", headers=headers).text)
-for team_info in event_info:
-    key = team["key"]
-    
+for event in range(2, events + 1):
+    event_id = input("Event ID #" + str(event) + " (including year): ")
+    event_info = loads(requests.get(base_url + "events/" + event_id + "teams", headers=headers).text)
+    for team_info in event_info:
+        key = team["key"]
+        if key in keys:
+            keys.add(key)
+            teams.append(get_team_history(key))
+
